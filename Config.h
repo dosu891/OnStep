89ac3c15cc4f071b2e79902a520971e92c8616a6
dosu891 <95360947+dosu891@users.noreply.github.com>
2025-11-07 23:07:19 +0100

    Update Config.h

--- Config.h
+++ Config.h
@@ -251,7 +251,7 @@
 
 
 // AUXILIARY ADDON FEATURE CONTROL ------------------------------ @DS -- ALSO UPDATE OnStep\addons\Ethernet\Config.h
-#define ADDON_FEATURE1_NAME               "FAN" // "FE..", Name of feature being controlled.                                        Adjust
+#define ADDON_FEATURE1_NAME         "FAN_ONDER" // "FE..", Name of feature being controlled.                                        Adjust
 #define ADDON_FEATURE1_PURPOSE           SWITCH //    OFF, SWITCH, DIGITAL_IN                                                       Option
 #define ADDON_FEATURE1_DEFAULT_VALUE        OFF //    OFF, ON                                                                       Adjust
 #define ADDON_FEATURE2_NAME          "TEL OPEN" // "FE..", Name of feature being controlled.                                        Adjust
@@ -284,8 +284,8 @@
 #define ADDON_FEATUREB_NAME    "HEATER_COMA_ON" // "FE..", Name of feature being controlled.                                        Adjust
 #define ADDON_FEATUREB_PURPOSE           SWITCH //    OFF, SWITCH, DIGITAL_IN                                                       Option
 #define ADDON_FEATUREB_DEFAULT_VALUE        OFF //    OFF, ON                                                                       Adjust
-#define ADDON_FEATUREC_NAME          "FEATUREC" // "FE..", Name of feature being controlled.                                        Adjust
-#define ADDON_FEATUREC_PURPOSE              OFF //    OFF, SWITCH, DIGITAL_IN                                                       Option
+#define ADDON_FEATUREC_NAME         "FAN_BOVEN" // "FE..", Name of feature being controlled.                                        Adjust
+#define ADDON_FEATUREC_PURPOSE           SWITCH //    OFF, SWITCH, DIGITAL_IN                                                       Option
 #define ADDON_FEATUREC_DEFAULT_VALUE        OFF //    OFF, ON                                                                       Adjust
 #define ADDON_FEATURED_NAME          "FEATURED" // "FE..", Name of feature being controlled.                                        Adjust
 #define ADDON_FEATURED_PURPOSE              OFF //    OFF, SWITCH, DIGITAL_IN                                                       Option
